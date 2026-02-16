b1c18a2bdfd51d7a953128571cee2cdd1001185c
Senku <senku@Senku-01.Senku>
2026-02-16 19:59:49 +0700

    chore: suppress font lint warning and finalize documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,7 @@ export default async function RootLayout({
   return (
     <html lang="id" className="overflow-x-hidden" suppressHydrationWarning>
       <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
