68d2b7e9ed30d7bba37a607e36f72bc14d147a80
Senku <senku@Senku-01.Senku>
2026-02-16 19:58:47 +0700

    fix: use more comprehensive material symbols link for better icon support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default async function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
         />
       </head>
       <body
