a7692bc931ea070b8f8a3c2e804202a9e33b08fe
Senku <senku@Senku-01.Senku>
2026-03-04 12:29:54 +0700

    hide footer on mobile screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export default function Footer({ siteSettings }: FooterProps) {
   }
   
   return (
-    <footer className="bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
+    <footer className="hidden md:block bg-zinc-50 text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
       <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-white/10 lg:justify-between">
         <div className="me-12 hidden lg:block">
           <span>{t('footer.social_connect') || 'Get connected with us on social networks:'}</span>
